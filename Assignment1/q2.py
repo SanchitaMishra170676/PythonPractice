36ad9a6ed891f6637556442a316a91a108cd65e9
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2023-01-28 18:19:04 +0530

    Assignment1

---
+++ Assignment1/q2.py
@@ -0,0 +1,3 @@
+# install playsound
+from playsound import playsound
+playsound("D:\\2023\\Python-Practice\\Assignment1\\inspire.mp3")
---
+++ Assignment1/q3.py
@@ -0,0 +1,4 @@
+# install pillow
+from PIL import Image
+im = Image.open("d:\\2023\\Python-Practice\\Assignment1\\sign.jpeg")
+im.show()
