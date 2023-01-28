553496ab6dcad72398dc8ee20fa790bc608f9a8e
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2023-01-28 22:32:18 +0530

    Assignent4

---
+++ Assignment4/q1.py
@@ -0,0 +1,6 @@
+m1 = int (input("Enter marks fir subject 1 "))
+m2 = int (input("Enter marks fir subject 2 "))
+m3 = int (input("Enter marks fir subject 3 "))
+listMarks = [m1,m2,m3]
+listMarks.sort()
+print(listMarks)
---
+++ Assignment4/q2.py
@@ -0,0 +1,3 @@
+checkTuple = (1,20)
+print(checkTuple[0])
+# checkTuple[0] = 5 no allowed (immutable)
